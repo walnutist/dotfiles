f7ef7f8d03954fe330c533cd62149f4d1499c986
walnutist <hank.chong@icloud.com>
2026-07-01 22:34:34 +0800

    Add new briefcase-blank icon for macOS folder icons

--- dot_config/macos/setup.sh
+++ dot_config/macos/setup.sh
@@ -2,9 +2,9 @@ mkdir -p ~/Codes.localized/.localized
 mkdir -p ~/Knowledge.localized/.localized
 mkdir -p ~/Works.localized/.localized
 
-ln -s ./folders.zh_CN.strings ~/Codes.localized/.localized/zh_CN.strings
-ln -s ./folders.zh_CN.strings ~/Knowledge.localized/.localized/zh_CN.strings
-ln -s ./folders.zh_CN.strings ~/Works.localized/.localized/zh_CN.strings
+ln -s $(pwd)/folders.zh_CN.strings ~/Codes.localized/.localized/zh_CN.strings
+ln -s $(pwd)/folders.zh_CN.strings ~/Knowledge.localized/.localized/zh_CN.strings
+ln -s $(pwd)/folders.zh_CN.strings ~/Works.localized/.localized/zh_CN.strings
 
 folderify ./folder-icons/solid/briefcase.png ~/Works.localized
 folderify ./folder-icons/solid/books.png ~/Knowledge.localized
